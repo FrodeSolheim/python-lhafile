f1e8a9dc7b80eaf1e06eff242fb1bbae5d722ad9
Frode Solheim <frode-code@fsolheim.net>
2017-12-06 22:26:04 +0100

    Minor changes to prepare source for pypi

--- setup.py
+++ setup.py
@@ -24,23 +24,22 @@ lzhlib = Extension('lzhlib',
 
 setup(name="lhafile",
       packages=['lhafile'],
-      version='0.2.1',
+      version='0.2.2',
       description="LHA(.lzh) file extract interface",
       long_description="""Extract LHA(.lzh) file extension.
-Its interface is likely zipfile extension is included in regular
-python distribution.""",
-      author='Hidekazu Ohnishi',
-      author_email='the-o@neotitans.net',
-      url='http://trac.neotitans.net/wiki/lhafile',
-      download_url='http://trac.neotitans.net/wiki/lhafile',
+The interface is similar to the zipfile module in the regular
+Python distribution.""",
+      author='Frode Solheim',
+      author_email='frode-code@fengestad.no',
+      url='https://fengestad.no/python-lhafile/',
+      download_url='https://fengestad.no/python-lhafile/',
       license='BSD',
       keywords = [],
       classifiers = [
         'Development Status :: 4 - Beta',
-        'Envrionment :: Other Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent'
-        'Programming Language :: Python',
-        'Topic :: Software Development'],
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: System :: Archiving'],
       ext_modules=[lzhlib])
